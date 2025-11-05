e4322f23bd79d645724ed80ae09188baf6c5614b
Lazaro Fernandez Peinado <alejandro213341212@gmail.com>
2025-11-04 19:12:37 -0500

    Some keybinds and some changes to undo tree

--- lua/Alejandro/core_settings/keymaps.lua
+++ lua/Alejandro/core_settings/keymaps.lua
@@ -24,8 +24,36 @@ vim.api.nvim_create_autocmd("TextYankPost", {
 })
 
 -- Copy filepath to the clipboard
--- --vim.keymap.set("n", "<leader>fp", function()
--- 	local filePath = vim.fn.expand("%:~") -- Gets the file path relative to the home directory
--- 	vim.fn.setreg("+", filePath) -- Copy the file path to the clipboard register
--- 	print("File path copied to clipboard: " .. filePath) -- Optional: print message to confirm
--- end, { desc = "Copy file path to clipboard" })
+vim.keymap.set("n", "<leader>pp", function()
+	local filePath = vim.fn.expand("%:~") -- Gets the file path relative to the home directory
+	vim.fn.setreg("+", filePath) -- Copy the file path to the clipboard register
+	print("File path copied to clipboard: " .. filePath) -- Optional: print message to confirm
+end, { desc = "Copy file path to clipboard" })
+
+
+-- Move between splits with Alt + h/j/k/l
+vim.keymap.set("n", "<A-h>", "<C-w>h", { noremap = true, silent = true })
+vim.keymap.set("n", "<A-j>", "<C-w>j", { noremap = true, silent = true })
+vim.keymap.set("n", "<A-k>", "<C-w>k", { noremap = true, silent = true })
+vim.keymap.set("n", "<A-l>", "<C-w>l", { noremap = true, silent = true })
+
+-- Open vertical split with Alt + o
+vim.keymap.set("n", "<A-o>", ":vsplit<CR>", { noremap = true, silent = true })
+
+-- Open horizontal split with Alt + i
+vim.keymap.set("n", "<A-i>", ":split<CR>", { noremap = true, silent = true })
+
+-- Close all splits except current with Alt + p
+vim.keymap.set("n", "<A-p>", "<C-w>o", { noremap = true, silent = true })
+
+-- Resize
+vim.keymap.set("n", "<A-=>", "<C-w>=", { silent = true, desc = "Equalize splits" })
+vim.keymap.set("n", "<A-.>", "<C-w>>", { silent = true })
+vim.keymap.set("n", "<A-,>", "<C-w><", { silent = true })
+vim.keymap.set("n", "<A-m>", "<C-w>|", { silent = true, desc = "Maximize width" })
+
+-- Swap splits (Alt + Shift + h/j/k/l)
+vim.keymap.set("n", "<A-S-h>", "<C-w>H", { silent = true, desc = "Swap split left" })
+vim.keymap.set("n", "<A-S-j>", "<C-w>J", { silent = true, desc = "Swap split down" })
+vim.keymap.set("n", "<A-S-k>", "<C-w>K", { silent = true, desc = "Swap split up" })
+vim.keymap.set("n", "<A-S-l>", "<C-w>L", { silent = true, desc = "Swap split right" })
--- lua/Alejandro/plugins/undotree.lua
+++ lua/Alejandro/plugins/undotree.lua
@@ -5,5 +5,11 @@ return {
 			vim.cmd.UndotreeToggle()
 			vim.cmd.UndotreeFocus()
 		end)
+        -- Enable dynamic updates / live changes
+        vim.g.undotree_ShortIndicators = 1          -- show changed text indicator
+        vim.g.undotree_SetFocusWhenToggle = 1       -- focus on toggle
+        vim.g.undotree_DiffAutoRefresh = 1          -- refresh diffs dynamically
+        vim.g.undotree_WindowLayout = 2             -- vertical split (optional)
+        vim.g.undotree_SplitWidth = 35              -- width of the tree
 	end,
 }
